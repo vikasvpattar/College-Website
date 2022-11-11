eb6304f87b3e1282469b36fcb47d46b9f4df8145
Vikas V Pattar <81294766+vikasvpattar@users.noreply.github.com>
2022-11-11 10:57:28 +0530

    footer update for mobile

--- main.js
+++ main.js
@@ -24,8 +24,25 @@ closebtn.addEventListener("click", () => {
 
 // slide
 let flag=0;
+function controller(x){
+  flag=flag+x;
+  slideshow(flag);
+}
 slideshow(flag);
-function slideshow(){
-  let slide= document.getElementsByClassName("slide");
-  console.log(slide);
+function slideshow(num){
+  let slides= document.getElementsByClassName("slide");
+  if(num==slides.length){
+    flag=0;
+    num=0;
+  }
+  if(num<0){
+    flag=slides.length-1;
+    num=slides.length-1;
+  }
+
+  console.log(slides);
+  for(let y of slides){
+    y.style.display="none";
+  }
+  slides[num].style.display="block";
 }
